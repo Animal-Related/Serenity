e49223a536679f50178ac0e184219df4d9f081f1
Louis Pilfold <louis@lpil.uk>
2014-10-18 18:54:40 +0100

    js loadsounds use map

--- public/js/serenity.js
+++ public/js/serenity.js
@@ -1,14 +1,17 @@
 (function() {
   var loadSounds = function loadSounds(notes, soundName) {
     // Takes:
-    //  * an array of ints that represent the notes we want to download,
+    //  * an array of notes that represent the notes we want to download.
+    //    i.e. "4-00" for C 4
+    //         "4-01" for C#4
+    //         "5-01" for D 5
     //  * the name of the sound. audio file format = {soundname}-{num}.ogg
     // Returns
     //  * an array of HTML audio elements of each of those notes
 
     var audioElem,
         filename,
-        audios = [];
+        audios = {};
 
     for (var i = 0, len = notes.length; i < len; i++) {
       filename = 'public/sounds/' + soundName + '-' + notes[i] + '.ogg';
